d35d924170109a386d2b13efd54cadecc35ac1b5
Heather <hpo@odoo.com>
2021-05-03 16:53:49 -0700

    [UPDATE] manifest information for addon

---
+++ auto_calculated_price/__manifest__.py
@@ -0,0 +1,27 @@
+# -*- coding: utf-8 -*-
+
+{
+    'name': 'auto-calculated-price',
+
+    'summary': """Adds functionality to auto calculate price.""",
+
+    'description': """
+        Uses Pair per Case X Price per Pair to calculate Sales Price
+    """,
+
+    'author': 'HPO',
+
+    'category': 'Sales',
+
+    'version': '1.0',
+
+    'depends': ['base', 'product'],
+
+    'data': [
+
+    ],
+
+    'demo': [
+
+    ],
+}
